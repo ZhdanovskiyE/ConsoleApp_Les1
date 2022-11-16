0c6920cd63c47b336c7e9801e1201c1219bb174e
ZhdanovskiyE <58247478+ZhdanovskiyE@users.noreply.github.com>
2022-11-16 11:21:06 +0500

    Сделал

--- ConsoleApp_Les1/Program.cs
+++ ConsoleApp_Les1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lesson_1
+{
+	class Program
+	{
+		static void Main()
+		{
+			string path = "D:/asd1.txt";
+			string text = "Программа завершилась, пользователь написал правильный ответ."; 
+
+			Console.WriteLine("Напишите ответ на главный вопрос жизни, вселенной и всего такого.");
+			int Anfr = int.Parse(Console.ReadLine());
+			if (Anfr == 42)
+			{
+				Console.WriteLine("Совершенно верно!");
+			}
+			else
+            {
+				while (Anfr != 42)
+				{ 					
+					Console.WriteLine("Всем известно что ответ на гланвый вопрос жизни, вселенной и всего такого - 42");
+					Anfr = int.Parse(Console.ReadLine());
+				}
+				Console.WriteLine("Поздравляю!");
+			}
+			using (FileStream fstream = new FileStream(path, FileMode.Create))
+			{
+				using (StreamWriter output = new StreamWriter(fstream))
+				{
+					output.Write(text);
+				}
+			}
+
+		}
+	}
+}
